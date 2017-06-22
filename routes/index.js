3ddba5078a8d414359e6655c460c4c2a2552dca9
DeepikaDhal <dhaldeepika48@gmail.com>
2017-06-22 17:30:46 +0530

    huhfi

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+var URL="mongodb://dpi:123456@ds131782.mlab.com:31782/dp"
+
+const db=require('monk')(URL)
+
+const docs=db.get('dpu')
+/* GET home page. */
+router.get('/welcome', function(req, res, next) {
+  //res.render('index', { title: 'Express' });
+  docs.insert({"name":"mona","age":"19"},function (err,docs) {
+      if(err) console.log(err)
+      else res.send('success');
+
+  })
+
+});
+
+router.get('/wel', function(req, res, next) {
+    res.send('deepi');
+});
+module.exports = router;
