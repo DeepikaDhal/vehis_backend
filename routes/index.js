5c77c85c69dffc1b1e088722760cec5102d2743f
DeepikaDhal <dhaldeepika48@gmail.com>
2017-06-23 17:23:23 +0530

    hsvdg

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,11 @@ var URL="mongodb://dpi:123456@ds131782.mlab.com:31782/dp"
 
 const db=require('monk')(URL)
 
-const docs=db.get('dpu')
+const docs=db.get('d')
 /* GET home page. */
-router.get('/welcome', function(req, res, next) {
+router.get('/push', function(req, res, next) {
   //res.render('index', { title: 'Express' });
-  docs.insert({"name":"mona","age":"19"},function (err,docs) {
+  docs.update({"id":"5e438","name":"deepika"},{$push:{"group":{"name":"user1"}}},function (err,docs) {
       if(err) console.log(err)
       else res.send('success');
 
